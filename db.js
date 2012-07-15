0daded3b8e517f444c40abd74b73bb52159caf6a
Jr <jr@3on.fr>
2012-07-15 15:21:57 -0700

    let's bootstrap with even more code

--- db.js
+++
@@ -1,6 +0,0 @@
-exports.getUserById = function(id, callback) {
-	// query with id and then call the callback
-	var queryResult = {login: 'John Doe', pass: 'toto'};
-	console.log("-> getUserById with id="+id);
-	callback(queryResult);
-};
